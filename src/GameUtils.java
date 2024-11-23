ab6563957db5b83a19bd130067e1f5cdc6ed0bf5
Vanshuman <anshuman.vishen@repute.net>
2024-11-23 12:18:46 +0530

    Adding Basic Util functions in GamesUtil

--- src/GameUtils.java
+++ src/GameUtils.java
@@ -1,3 +1,21 @@
 public class GameUtils {
 
+    public static int getDiceRoll() {
+        return (int) (Math.random() * 6) + 1;
+    }
+
+    public static int getAttackDamage(Player attacker, int roll) {
+        return attacker.getAttack() * roll;
+    }
+
+    public static int getDefendingStrength(Player defender, int roll) {
+        return defender.getStrength() * roll;
+    }
+
+    public static int getHealthDamage(int attackDamage, int defendingStrength) {
+        if (attackDamage > defendingStrength) {
+            return attackDamage - defendingStrength;
+        }
+        return 0;
+    }
 }
